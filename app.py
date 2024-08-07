eddefaf3ff5535fbd017ba18c521c0120f9b4778
karthickeyan17 <skarthickeyan707@gmail.com>
2024-08-07 20:29:39 +0530

    modified app and model code

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def get_user_input():
 
 # Main function to run the Streamlit app
 def app():
-    st.title("Obesity Prediction App")
+    st.markdown("# Obesity Prediction App", unsafe_allow_html=True)
     
     # Get user input
     user_input = get_user_input()
@@ -42,16 +42,23 @@ def app():
     # Model selection
     model_choice = st.sidebar.selectbox('Choose Model', ('Logistic Regression', 'SVM'))
     
+    img_urls = [
+        'https://img.freepik.com/free-vector/hand-drawn-strong-man-cartoon-illustration_52683-117786.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1722902400&semt=ais_hybrid',
+        'https://img.freepik.com/free-vector/overweight-man-eating-fast-food-table-isolated_1308-133546.jpg',
+        'https://www.fitterfly.com/blog/wp-content/uploads/2021/09/overweight-in-child-1200x720.jpg',
+        'https://static1.srcdn.com/wordpress/wp-content/uploads/2021/01/the-machinist-christian-bale-weight-loss.jpg'
+    ]
+    
     # Prediction button
     if st.button('Predict'):
         if model_choice == 'Logistic Regression':
             prediction = lr_model.predict(user_input)
         elif model_choice == 'SVM':
             prediction = svm_model.predict(user_input)
-        labels = ['Normal weight', 'Obese', 'Overweight', 'Underweight']
         
-        st.subheader(f'{model_choice} Prediction:')
-        st.write(labels[prediction[0]])
+        labels = ['Normal weight', 'Obese', 'Overweight', 'Underweight']
+        st.subheader(f'{model_choice} Prediction: {labels[prediction[0]]}')
+        st.image(img_urls[prediction[0]])
 
 if __name__ == '__main__':
     app()
